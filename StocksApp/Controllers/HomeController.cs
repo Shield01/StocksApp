de85837b84a062f8e007e26870e53b8dc1ef1fa3
User <User@WINDOWS-LCNFO2I>
2025-06-07 17:49:55 +0100

    Add project files.

---
+++ StocksApp/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StocksApp.Services;
+
+namespace StocksApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly MyService _myService;
+        public HomeController(MyService myservice) {
+            _myService = myservice;
+        }
+        [Route("/")]
+        public async Task<IActionResult> Index()
+        {
+            await _myService.method();
+
+            return View();
+        }
+    }
+}
---
+++ StocksApp/Services/MyService.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+
+namespace StocksApp.Services
+{
+    public class MyService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public MyService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task method()
+        {
+            using (HttpClient httpClient = _httpClientFactory.CreateClient())
+            {
+                HttpRequestMessage httpRequestMessage = new HttpRequestMessage()
+                {
+                    RequestUri = new Uri("https://finnhub.io/api/v1/quote?symbol=MSFT&token=d125ah1r01qmhi3gh69gd125ah1r01qmhi3gh6a0"),
+                    Method = HttpMethod.Get
+                };
+
+                HttpResponseMessage httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+
+                Stream stream = httpResponseMessage.Content.ReadAsStream();
+
+                StreamReader streamReader = new StreamReader(stream);
+
+                string responseBody = streamReader.ReadToEnd();
+
+                Dictionary<string, object>? responseDictionary = JsonSerializer.Deserialize<Dictionary<string, object>>(responseBody);
+            }
+        }
+    }
+}
